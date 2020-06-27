8929f761e05939ce93651396e120752e9dc8f874
Rahul Nag <rahulnag625@gmail.com>
2020-06-27 09:05:10 +0530

    gitignore file edited and build folder added in gitignore

---
+++ build/precache-manifest.799efe614845910780a8d13feaf9feb6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e428edf618eb8d7523d0df6edd2bb706",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9182fa60ec9ba27b4b8e",
+    "url": "/static/css/main.b9642b2f.chunk.css"
+  },
+  {
+    "revision": "2d580ac041fcdf105f5b",
+    "url": "/static/js/2.ff2aa268.chunk.js"
+  },
+  {
+    "revision": "1020e02bdad48d4e8e368ed0676862a8",
+    "url": "/static/js/2.ff2aa268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9182fa60ec9ba27b4b8e",
+    "url": "/static/js/main.771b5d53.chunk.js"
+  },
+  {
+    "revision": "3bd790328d98d3f02ece",
+    "url": "/static/js/runtime-main.7a562d59.js"
+  },
+  {
+    "revision": "d186612d1a1d3b4b7195f6523f3d9903",
+    "url": "/static/media/github.d186612d.svg"
+  },
+  {
+    "revision": "b0cbe41d5658fdacc6b6d9b1447cc35d",
+    "url": "/static/media/html5_css3.b0cbe41d.png"
+  },
+  {
+    "revision": "4f9a90169186fcb76c7560b29c965f1b",
+    "url": "/static/media/innovation_.4f9a9016.png"
+  },
+  {
+    "revision": "d2c78eab408a616010a54aa4a7700f22",
+    "url": "/static/media/javascript.d2c78eab.png"
+  },
+  {
+    "revision": "27da0519ef0ba1e8a45937ac22585589",
+    "url": "/static/media/mohit.27da0519.png"
+  },
+  {
+    "revision": "08ebf3d0bdc551aa5861bb101c767dd2",
+    "url": "/static/media/node.08ebf3d0.png"
+  },
+  {
+    "revision": "ad4f1a81bb5139a075ca4a299039c646",
+    "url": "/static/media/priyanka.ad4f1a81.png"
+  },
+  {
+    "revision": "a31198d15aeefb126a0155c626aa6826",
+    "url": "/static/media/rahul.a31198d1.png"
+  },
+  {
+    "revision": "638be90904f9f9248d3c69cb2992c562",
+    "url": "/static/media/reactjs.638be909.png"
+  },
+  {
+    "revision": "f3f9d9501ae3b221764346be36517e78",
+    "url": "/static/media/reactnative.f3f9d950.png"
+  },
+  {
+    "revision": "34ccee7a81af2113fa9311fa220f0cb3",
+    "url": "/static/media/wave.34ccee7a.svg"
+  },
+  {
+    "revision": "553f00438cefc583f7ae094baec0e959",
+    "url": "/static/media/wave.553f0043.jpg"
+  }
+]);
